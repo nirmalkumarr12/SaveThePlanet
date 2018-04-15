83957dbfae43951646583a30b7fcecb6d4648cc8
unknown <nirmalkumar.r12@gmail.com>
2018-04-14 20:14:15 -0500

    Added splash,starter and signup screen. color resource purple value set

---
+++ app/src/main/java/savetheplanet/team8/project/com/savetheplanet/SignUpActivity.java
@@ -0,0 +1,13 @@
+package savetheplanet.team8.project.com.savetheplanet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
