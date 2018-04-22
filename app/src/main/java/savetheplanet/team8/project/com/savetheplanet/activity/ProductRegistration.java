0ed8617554a031d934e17b1ec8a3180fe1615af7
unknown <nirmalkumar.r12@gmail.com>
2018-04-22 13:24:22 -0500

    Product Registration Activity added

---
+++ app/src/main/java/savetheplanet/team8/project/com/savetheplanet/activity/ProductRegistration.java
@@ -0,0 +1,78 @@
+package savetheplanet.team8.project.com.savetheplanet.activity;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mehdi.sakout.fancybuttons.FancyButton;
+import savetheplanet.team8.project.com.savetheplanet.R;
+import savetheplanet.team8.project.com.savetheplanet.model.Product;
+
+public class ProductRegistration extends BaseActivity {
+    TextView displayName;
+    TextInputLayout firstNameLayout, lastNameLayout, companyNameLayout;
+    TextInputEditText productName, productDescription, productLocation,productTag;
+    FloatingActionButton doneButton;
+    FancyButton galleryButton, cameraButton, removeButton;
+    CircularImageView imageView;
+    private StorageReference storageReference;
+    DatabaseReference databaseReference;
+    String userId;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_registration);
+        String userId = "RokYULCU3KSwlfhYoCW8OlnOhlq2";
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        doneButton=(FloatingActionButton)findViewById(R.id.doneButton);
+
+        productName= (TextInputEditText) findViewById(R.id.productNameTextEditText);
+
+        productDescription= (TextInputEditText) findViewById(R.id.productDescriptionTextEditText);
+        productLocation = (TextInputEditText) findViewById(R.id.productlocation);
+        productTag= (TextInputEditText) findViewById(R.id.productTagTextEditText);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerProduct();
+            }
+        });
+    }
+    public void registerProduct(){
+        String name=productName.getText().toString();
+        String description=productDescription.getText().toString();
+        String location=productLocation.getText().toString();
+        String tag=productTag.getText().toString();
+        Product product=new Product(name,description,location,tag);
+
+        //Firebase newRequestRef = mDatabase.child("request").push();
+            List<Product> plist=new ArrayList<Product>();
+
+
+
+            DatabaseReference dr=databaseReference.child("products").push();
+            dr.setValue(product);
+
+
+        }
+
+
+}
